fb617d2aa0df8dd74997f19a5e47808fb1839e12
Jonathan Parker <joparker@penguinrandomhouse.com>
2023-09-11 08:53:58 -0400

    Initial content

---
+++ os-module/src/application.mjs
@@ -0,0 +1,34 @@
+/**
+ * (#)application.mjs   1.0.0   09/11/2023
+ *
+ * Copyright (c) Jonathan M. Parker
+ * 324 Lantana Drive
+ * Owings Mills, MD  21117 U.S.A
+ * All Rights Reserved.
+ */
+
+//import { PathModule} from "./path-module.mjs";
+
+/**
+ * The application class.
+ */
+export class Application {
+    /**
+     * The run method.
+     */
+    run() {
+        console.log('Good morning');
+
+        /*
+        let pathModule = new PathModule(config);
+
+        pathModule.pathProperties();
+
+        try {
+            pathModule.pathMethods();
+        } catch (error) {
+            console.error(error);
+        }
+         */
+    }
+}
